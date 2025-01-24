b59833c54c6a65912c1ae4702ff7bd870a8a0ee8
Jay <jordanbell321@gmail.com>
2025-01-24 14:03:56 +0000

    `Added dropdown menu functionality to user profile picture in navigation bar`

---
+++ js/navDropdown-script.js
@@ -0,0 +1,29 @@
+
+const profilePicture = document.getElementById('profile-picture');
+const dropdownMenu = document.getElementById('dropdown-menu');
+const logoutButton = document.getElementById('logout');
+
+// Toggle dropdown menu when clicking on the profile picture
+profilePicture.addEventListener('click', () => {
+    console.log('clicked');
+    dropdownMenu.classList.toggle('show');
+});
+
+// Hide dropdown menu if clicking outside of it
+document.addEventListener('click', (e) => {
+    if (!dropdownMenu.contains(e.target) && e.target !== profilePicture) {
+        dropdownMenu.classList.add('hidden');
+        dropdownMenu.classList.remove('show');
+    }
+});
+
+// Logout logic
+logoutButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    // Clear token from localStorage
+    localStorage.removeItem('auth_token');
+
+    // Redirect to homepage or login page
+    window.location.href = 'https://jaynightmare.github.io/TALE-FYP/screens/homepage/index.html';
+});
+
