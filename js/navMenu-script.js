4c438688f6d0b1fe6fe59a4a8ef87b29347bca54
Jay <jordanbell321@gmail.com>
2025-01-31 16:59:52 +0000

    d

--- js/navMenu-script.js
+++ js/navMenu-script.js
@@ -1,4 +1,6 @@
 const navMenu = document.getElementById("navBarMenu");
 const navOptions = document.getElementById("navbar-options");
 
-navMenu.addEventListener("click", () => { navOptions.classList.toggle("block-layout-inject"); });
+navMenu.addEventListener("click", () => {
+    navOptions.classList.toggle("block-layout-inject");
+});
